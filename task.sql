9cfd6ee7bc2c335cd62db288693a9800b87fdff3
Shershen Oleksandra <o_shershen@viyar.ua>
2026-04-13 15:32:39 +0300

    feat(task.sql): add procedure

--- task.sql
+++ task.sql
@@ -1,3 +1,17 @@
-USE ShopDB; 
+USE ShopDB;
 
--- Create your stored procedure here
+DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
+
+DELIMITER $$
+
+CREATE PROCEDURE get_warehouse_product_inventory(IN warehouse_id INT)
+BEGIN
+    SELECT
+        p.Name AS ProductName,
+        pi.WarehouseAmount
+    FROM ProductInventory pi
+    JOIN Products p ON p.ID = pi.ProductID
+    WHERE pi.WarehouseID = warehouse_id;
+END$$
+
+DELIMITER ;
